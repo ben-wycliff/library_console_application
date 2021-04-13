c265f7638bf0c521682e100167e569cef93e3802
ben-wycliff <ben12wycliff@gmail.com>
2021-04-13 14:18:06 +0300

    Retrieves all books

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from pymongo import MongoClient
 
 
 def connect_to_database():
-    """This function connects to the mongoDB running instance"""
     URI = "mongodb://127.0.0.1:27017/"
     client = MongoClient(URI)
-    print("=================== Connection successful ===================\n")
+    print("\n===================== connection successful =====================\n")
 
 def print_options():
     print("""
@@ -15,13 +14,17 @@ def print_options():
           3. Delete a book
           4. Edit a book
           """)
-
+    
 while True:
     connect_to_database()
     print_options()
     response = int(input())
+    
     if response == 1:
-        print("Getting all books")
+        books = client.newDB.newCollection.find_one()
+        print("Books:\n")
+        print(books)
+        
     elif response == 2:
         print("Adding a book")
     elif response == 3:
