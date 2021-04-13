81fbf3a1052c1ade4d07c1a47b483412720f9810
ben-wycliff <ben12wycliff@gmail.com>
2021-04-13 14:46:21 +0300

    Add and delete book from database

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from pymongo import MongoClient
 
 
-def connect_to_database():
-    URI = "mongodb://127.0.0.1:27017/"
-    client = MongoClient(URI)
-    print("\n===================== connection successful =====================\n")
+
+URI = "mongodb://127.0.0.1:27017/"
+client = MongoClient(URI)
+print("\n===================== connection successful =====================\n")
 
 def print_options():
     print("""
@@ -12,25 +12,40 @@ def print_options():
           1. Get all books
           2. Add a book
           3. Delete a book
-          4. Edit a book
           """)
     
+def print_add_book():
+    print("Enter title:\n")
+    title = str(input())
+    print("Enter author: \n")
+    author = str(input())
+    return title, author
+
+def print_delete_book():
+    print("Enter book title:\n")
+    title = str(input())
+    return title
+
 while True:
-    connect_to_database()
     print_options()
     response = int(input())
     
     if response == 1:
-        books = client.newDB.newCollection.find_one()
+        books = client.library.books.find({})
         print("Books:\n")
-        print(books)
+        for i, book in enumerate(books):
+            print(str(i+1)+"\t"+book["title"])
         
     elif response == 2:
-        print("Adding a book")
+        title, author = print_add_book()
+        client.library.books.insert_one({"title": title, "author": author})
+        print("Book titled " + title + " has been registered successfully.")
+        
     elif response == 3:
-        print("Deleting a book")
-    elif response == 4:
-        print("Editing a book")
+        title = print_delete_book()
+        client.library.books.delete_one({"title": title})
+        print("Book titled "+ title + " has been deleted.")
+        
     else:
         print("Exiting library")
         client.close()
